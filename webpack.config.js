60aa1141a6039c6bc7b3b54e7ad2f1e00df01cec
Olerato. <olerato.mosikidi@gmail.com>
2025-03-24 19:22:51 +0200

    Change mode to 'development'.

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
