f1b25990939d4a908dba1430d62459ec33ecc958
Olerato. <olerato.mosikidi@gmail.com>
2025-03-24 21:48:34 +0200

    Create addProject, getProjects and getProject methods to TodoList class.

--- src/modules/TodoList.js
+++ src/modules/TodoList.js
@@ -6,4 +6,18 @@ export default class TodoList{
     this.projects.push(new Project('Personal')),
     this.projects.push(new Project('Work'))
   }
+  
+  addProject(project){
+    if(this.projects.every((p) => p.name !== project.name))
+    this.projects.push(project)
+    else console.log('Project already exists!')
+  }
+  
+  getProjects(){
+    return this.projects
+  }
+  
+  getProject(projectName){
+    return this.projects.find((p) => p.name === projectName)
+  }
 }
