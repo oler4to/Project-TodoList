2c66a019771ef2dcbf2cfb57c9cc6651d7b114fb
Olerato. <olerato.mosikidi@gmail.com>
2025-03-24 20:04:03 +0200

    Create Task class.

---
+++ src/modules/Task.js
@@ -0,0 +1,16 @@
+
+export default class Task{
+  constructor(
+    name, 
+    details = 'No Details', 
+    duedate = 'No Date', 
+    urgency = 'Low', 
+    project = 'None')
+    {
+    this.name = name,
+    this.details = details,
+    this.duedate = duedate,
+    this.urgency = urgency,
+    this.project = project
+  }
+}
