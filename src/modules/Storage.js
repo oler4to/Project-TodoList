1f73401bd8392a40c78bee25b4972431e8610e3c
Olerato. <olerato.mosikidi@gmail.com>
2025-03-25 19:25:15 +0200

    Create onStartup method that will set up the to-do list with some pre-existing task when visiting the site for the first time. The to-do list will then be saved to localStorage. Then created the saveToStorage method which will just serve as an updater for the to-do list in localStorage each time it is called. Finally, create the setupTodo method which will just retrieve the to-do list from localStorage.

--- src/modules/Storage.js
+++ src/modules/Storage.js
@@ -3,5 +3,58 @@ import Project from './Project.js'
 import TodoList from './TodoList.js'
 
 export default class Storage {
+  onStartup(){
+    if(localStorage.length < 1) {
+    const todo = new TodoList()
+    
+    todo
+    .getProject('Personal')
+    .addTask(
+      new Task(
+        'This is a random task',
+        'Remember to do some random thing!',
+        '25/03/2025',
+        'Medium',
+        'Personal'
+        ))
+    
+    todo
+    .getProject('Personal')
+    .addTask(
+      new Task(
+        'This is another random task',
+        'Remember to do some other random things!',
+        '26/03/2025',
+        'High',
+        'Personal'
+        ))
+    
+    todo
+    .getProject('Work')
+    .addTask(
+      new Task(
+        'This time its an IMPORTANT task for work',
+        'Remember to do some that very important thing for that one person!',
+        '29/03/2025',
+        'High',
+        'Work'
+        ))
+    
+    localStorage.setItem('todo', JSON.stringify(todo))
+    } else console.log('already done!')
+    
+  }
+  
+  saveToStorage(todo){
+    localStorage.setItem('todo', todo)
+  }
+  
+  setupTodo(){
+    const todo = Object.assign(
+      new TodoList, 
+      JSON.parse(localStorage.getItem('todo')))
+      
+  return todo
+  }
   
 }
