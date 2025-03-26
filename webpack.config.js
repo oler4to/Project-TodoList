2c94bfd5c88dcab6f74a906952ff7a2d5d6c90b7
Olerato. <olerato.mosikidi@gmail.com>
2025-03-26 18:24:09 +0200

    Importing CSS gave me SHIT so I uninstalled the loaders and got rid of the module configuration. Will try it again at later stage.

--- webpack.config.js
+++ webpack.config.js
@@ -9,14 +9,6 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
   devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
