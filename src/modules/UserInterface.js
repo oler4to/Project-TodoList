52e5c3e3234189fafc7b6ab3e46c0542e351d468
Olerato. <olerato.mosikidi@gmail.com>
2025-03-30 06:57:47 +0200

    Target DOM 'mainDisplay' in 'deleteTaskButton'. Clean up code for better readability.

--- src/modules/UserInterface.js
+++ src/modules/UserInterface.js
@@ -4,8 +4,13 @@ import TodoList from './TodoList.js'
 import Storage from './Storage.js'
 
 export default class UserInterface{
+  
+  
+  
   static sendFormData(form){
+    
     if(form[0].value !== "")
+    
    return Storage.addTask(
       form[4].value,
       form[0].value,
@@ -13,9 +18,12 @@ export default class UserInterface{
       form[2].value,
       form[3].value,
       )
+    
   }
   
-  static createTask(output,form){
+  
+  
+  static createTask(output, projectName, taskName, taskDetails){
     
     const project = Storage.getProject(form[4].value,);
     
@@ -27,49 +35,67 @@ export default class UserInterface{
       )
     
     const div = document.createElement('div')
+    
           div.setAttribute('id', 'task')
           
-        div.appendChild(
-          UserInterface.createTaskField(
-            task.name, 'task-name'))
-          
-        div.appendChild(
-          UserInterface.createTaskField(
-            task.details, 'task-details'))
-          
-        div.appendChild(
-          UserInterface.createTaskField(
-            task.duedate, 'task-duedate'))
-          
-        div.appendChild(
-          UserInterface.createTaskField(
-            task.urgency, 'task-urgency'))
-          
-        div.appendChild(
-          UserInterface.createTaskField(
-            task.project, 'task-project'))
+          div.appendChild(
+            UserInterface.createTaskField(
+              task.name, 'task-name'))
             
-        div.appendChild(UserInterface.deleteTaskButton(project, task))
+          div.appendChild(
+            UserInterface.createTaskField(
+              task.details, 'task-details'))
+            
+          div.appendChild(
+            UserInterface.createTaskField(
+              task.duedate, 'task-duedate'))
+            
+          div.appendChild(
+            UserInterface.createTaskField(
+              task.urgency, 'task-urgency'))
+            
+          div.appendChild(
+            UserInterface.createTaskField(
+              task.project, 'task-project'))
+              
+          div.appendChild(
+            UserInterface.deleteTaskButton(project, task))
     
     output.appendChild(div)
+    
   }
   
   static createTaskField(fieldValue, fieldID){
-    const span = document.createElement('span')
-          span.setAttribute('id', fieldID)
-          span.textContent = fieldValue
+    
+    const span = document.createElement('span');
+    
+          span.setAttribute('id', fieldID);
+          span.textContent = fieldValue;
           
-      return span
+    return span
+    
   }
   
   static deleteTaskButton(project, task){
+    
+    const mainDisplay = document.querySelector('#output')
+    
     const deleteButton = document.createElement('button')
+    
           deleteButton.setAttribute('id', 'task-delete-button')
           deleteButton.textContent = 'delete'
           deleteButton.onclick = () => {
-            Storage.deleteTask(project.name, task.name)
+          Storage.deleteTask(
+            project.name,
+            task.name ),
+            mainDisplay.removeChild(deleteButton.parentElement)
+  
           }
-          
-      return deleteButton
+    return deleteButton
+    
   }
+  
+  
+  
+  
 }
