537e0a398a9a893bbd980266e383010aeedd59dd
Olerato. <olerato.mosikidi@gmail.com>
2025-04-02 22:22:11 +0200

    Import functions from 'date-fns'.
    Minor changes to 'addTask' and 'deleteTask' methods.
    
    Declare 'getTasksDueToday', 'getTasksDueThisWeek' and 'getTasksOverdue' methods which will return a filterd tasks array of tasks that are due today, within the current week, and overdue, respectively.

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -1,3 +1,7 @@
+import {isWithinInterval, isToday, subHours, isBefore, addDays} from 'date-fns'
+import Storage from './Storage.js'
+
+
 export default class Project {
   constructor(name){
     this.name = name,
@@ -20,19 +24,56 @@ export default class Project {
     return this.tasks
   }
   
-  
   getTask(taskName){
     return this.tasks.find(task => 
     task.name == taskName)
   }
   
-  addTask(t){
-    if(this.tasks.every((task) => task.name != t.name)){
-      this.tasks.push(t)
+  addTask(task){
+    if(this.tasks.every((t) => t.name !== task.name)){
+      this.tasks.push(task)
     }
   }
   
   deleteTask(tName){
-    this.setTasks(this.tasks.filter((task) => task.name !== tName))
+   this.tasks = this.tasks.filter(task => task.name != tName)
   }
+  
+  getTasksDueToday(){
+    return this.tasks.filter( task => {
+      const taskDate = subHours(new Date(task.duedate), 2);
+      return isToday(taskDate) == true
+      })
+  }
+  
+  getTasksDueThisWeek(){
+    return this.tasks.filter( task => {
+      
+      const taskDate = subHours(new Date(task.duedate), 2);
+      
+      const currentDate = 
+      `${new Date().getFullYear()}-
+      ${new Date().getMonth() + 1}-
+      ${new Date().getDate()}`
+      
+      return isWithinInterval(taskDate, {
+        start: currentDate,
+        end: addDays(currentDate, 6)
+      }) == true
+      })
+  }
+  
+  getTasksOverdue(){
+    return this.tasks.filter( task => {
+      const taskDate = subHours(new Date(task.duedate), 2);
+      
+      const currentDate = 
+      `${new Date().getFullYear()}-
+      ${new Date().getMonth() + 1}-
+      ${new Date().getDate()}`
+      
+      return isBefore(taskDate, currentDate) == true
+      })
+  }
+  
 }
